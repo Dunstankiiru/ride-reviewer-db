bcbb97de8d02f185d501725ae2be819f60b04a7f
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-25 14:25:39 +0300

    updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use('/api', router);
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://muthonii-web.github.io'); 
+    res.header('Access-Control-Allow-Origin', 'https://Dunstankiiru.github.io'); // The URL you put here is for the web application that you have deployed using Github Pages
     res.header('Access-Control-Allow-Headers', '*');
     next();
 });
